4a444bf781e48714c512805c8a4fbbfcec860c5a
bricks4nick <njohn066@ucr.edu>
2023-02-24 23:01:52 -0800

    Player class test file

---
+++ TestFile.cpp
@@ -0,0 +1,9 @@
+// #include Player.h
+// #include <string>
+// #include <iostream>
+
+// using namespace std;
+
+// Player::Player(PlayerName name, PlayerType type, int healthm int attack) {
+//     //Player constructor code
+// }
