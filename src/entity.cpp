4ec81d4f3c61fe338bb8e26aee315430b2fd6722
Anonymous-Mouse <aqadolfoplata2013@gmail.com>
2023-03-03 13:47:58 -0800

    Update remote branch with entity.h and entity.cpp, tests included in enemy_monster_class

---
+++ src/entity.cpp
@@ -0,0 +1,30 @@
+
+#include "../header/entity.h"
+
+Entity::~Entity() {
+
+}
+
+string Entity::getName(){
+    return this->Name;
+}
+
+int Entity::getHealth(){
+    return this->Health;
+}
+
+bool Entity::isDead(){
+    return this->DeadState;
+}
+
+void Entity::setHealth(int hp){
+    this->Health = hp;
+       if(getHealth() <= 0){
+        this->DeadState = true;
+        this->Health = 0;
+    }
+}
+
+vector<StatusEffect> Entity::getEffects(){
+    return this->Effects;
+}
