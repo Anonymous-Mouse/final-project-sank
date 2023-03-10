fb8d950da5abc28549414823c121500705e7fe40
karyeet <28524112+karyeet@users.noreply.github.com>
2023-03-09 19:07:10 -0800

    tests file

--- tests/itemTests.cpp
+++ tests/itemTests.cpp
@@ -2,8 +2,17 @@
 #include "../header/Item.h"
 #include "../header/Key.h"
 #include "../header/Weapon.h"
-#include "../Consumable.h"
+#include "../header/Consumable.h"
 #include <iostream>
 
+class MockItem: public Item{
+    public:
+    bool use(Entity*) override{
+        return true;
+    }
+    MockItem(std::string Name): Item(Name){}
+}
 
-
+TEST(Item, Construct){
+    EXPECT_NO_DEATH(MockItem item("testName"));
+}
