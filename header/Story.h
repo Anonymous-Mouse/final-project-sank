d00bbcf966381dbbf370c96acf529220ca57cfa7
karyeet <28524112+karyeet@users.noreply.github.com>
2023-03-15 18:47:46 -0700

    Story header

---
+++ header/Story.h
@@ -0,0 +1,29 @@
+#ifndef STORY_H
+#define STORY_H 
+
+#include <string>
+#include <vector>
+
+#include "./Level.h"
+#include "./Player.h"
+#include "./StoryIO.h"
+#include "./Room.h"
+
+class Story{
+    private:
+        Level* level;
+        std::string storyName;
+        Player* player;
+        StoryIO io;
+
+    public:
+        void setPlayer(Player*);
+        void start();
+        void attackLoop(Enemy);
+        std::vector<std::vector<Room*>> getRoomsAdjacentToPlayer();
+
+        story(std::string storyName, Level level, StoryIO io);
+        
+}
+
+#endif
