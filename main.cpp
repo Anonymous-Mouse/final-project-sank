e173d0baa60dfa81e07f73935613be91fba9d4ea
karyeet <28524112+karyeet@users.noreply.github.com>
2023-03-17 12:07:58 -0700

    typos

--- main.cpp
+++ main.cpp
@@ -14,14 +14,14 @@ using namespace std;
 int main(){
 Enemy* ruthlessRat = new Enemy("Ruthless Rat", 45, 15, 13, 1);
 Enemy* causticRoach = new Enemy("Caustic Cockroach", 35, 17, 20, 1);
-Enemy* heinoushornet = new Enemy("Heinous Hornet", 42, 14, 7, 1 )
+Enemy* heinoushornet = new Enemy("Heinous Hornet", 42, 14, 7, 1 );
 
 vector<vector<Room*>> Asylum = {
    { nullptr,                           nullptr,                            new Room("Exit"),       nullptr},
    { nullptr,                           new Room("Hallway"),                new Room("Reception"),  new Room("Broom Closet")},
    { nullptr,                           new Room("Hallway", causticRoach, 90),  nullptr,          nullptr},
    { new Room("Cell2", heinoushornet, 100), new Room("Hallway"),                nullptr,          nullptr},
-   {    nullptr,                        new Room("Hallway", giantRat, 100), new Room("Hallway"),  nullptr},
+   {    nullptr,                        new Room("Hallway", ruthlessRat, 100), new Room("Hallway"),  nullptr},
    {    nullptr,                        nullptr,                            new Room("Cell"),      nullptr}
 
 };
