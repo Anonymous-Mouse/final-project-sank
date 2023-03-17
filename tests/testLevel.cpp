f5eca7f6cda57b18b3062362650795306dee14c0
karyeet <28524112+karyeet@users.noreply.github.com>
2023-03-17 02:48:46 -0700

    Update tests, simplify setRoom

--- tests/testLevel.cpp
+++ tests/testLevel.cpp
@@ -6,77 +6,111 @@
 
 using namespace std;
 
-class MockLevelTest : public testing::Test {
-protected:
-    void SetUp() override {
-        vector<vector<Room*>> roomVector {
-            {new Room(), nullptr, new Room()},
-            {new Room(), new Room(), nullptr},
-            {nullptr, new Room(), nullptr}
-        };
-        level = new Level(1, roomVector);
-    }
-
-    void TearDown() override {
-        for (auto row : level->roomVector) {
-            for (auto room : row) {
-                delete room;
-            }
-        }
-        delete level;
-    }
-
-    Level* level; 
+vector<vector<Room*>> roomVector {
+    {new Room("testRoom"), nullptr, new Room("testRoom")},
+    {new Room("testRoom"), new Room("testRoom"), nullptr},
+    {nullptr, new Room("testRoom"), nullptr}
 };
 
-TEST(LevelTest, GetDifficulty) {
-    EXPECT_EQ(level->getDifficulty(), 1);
+TEST(LevelTest, GetDifficulty)
+{
+    vector<vector<Room*>> roomVector{{}};
+    Level level(1, roomVector);
+    EXPECT_EQ(level.getDifficulty(), 1);
 }
 
-TEST(LevelTest, GetRoomAtValid) {
-    Room* room = level->getRoomAt(0, 0);
+TEST(LevelTest, GetRoomAtValid)
+{
+
+    vector<vector<Room*>> roomVector {
+        {new Room("testRoom"), nullptr, new Room("testRoom")},
+        {new Room("testRoom"), new Room("testRoom"), nullptr},
+        {nullptr, new Room("testRoom"), nullptr}
+    };
+    Level level(1, roomVector);
+    Room* room = level.getRoomAt(0, 0);
     EXPECT_NE(room, nullptr);
-    EXPECT_EQ(room->getName(), "");
-    EXPECT_EQ(level->exploredRooms[0][0], true);
+    EXPECT_EQ(room->getName(), "testRoom");
+    EXPECT_EQ(level.exploredRooms[0][0], true);
 }
 
-TEST(LevelTest, GetRoomAtInvalid) {
-    Room* rooms = level->getRoomAt(-1, 0);
-    EXPECT_EQ(room, nullptr);
-    room = level->getRoomAt(0, 3);
-    EXPECT_EQ(room, nullptr);
-    EXPECT_FALSE(level->getRoomAt[-1][0]);
-    EXPECT_FALSE(level->getRoomAt[0][3]);
+TEST(LevelTest, GetRoomAtInvalid)
+{
+
+    vector<vector<Room*>> roomVector {
+        {new Room("testRoom"), nullptr, new Room("testRoom")},
+        {new Room("testRoom"), new Room("testRoom"), nullptr},
+        {nullptr, new Room("testRoom"), nullptr}
+    };
+    Level level(1, roomVector);
+
+    EXPECT_DEATH(Room* room = level.getRoomAt(-1, 0), "Invalid coordinates"); 
+
 }
 
-TEST(LevelTest, IsThereRoomValidInvalid) {
-    EXPECT_TRUE(level->getRoomAt(0, 0));
-    EXPECT_TRUE(level->exploredRooms[0][0]);
-    EXPECT_TRUE(level->isThereRoom(1, 1));
-    EXPECT_TRUE(level->exploredRooms[1][1]);
-    EXPECT_FALSE(level->isThereRoom(-1, 0));
-    EXPECT_FALSE(level->isThereRoom(0, 3));
+TEST(LevelTest, IsThereRoomValidInvalid)
+{
+
+    vector<vector<Room*>> roomVector {
+        {new Room("testRoom"), nullptr, new Room("testRoom")},
+        {new Room("testRoom"), new Room("testRoom"), nullptr},
+        {nullptr, new Room("testRoom"), nullptr}
+    };
+    Level level(1, roomVector);
+
+    EXPECT_TRUE(level.getRoomAt(0, 0));
+    EXPECT_TRUE(level.exploredRooms[0][0]);
+
+    EXPECT_TRUE(level.isThereRoom(1, 1));
+    EXPECT_FALSE(level.exploredRooms[1][1]);
+
+    EXPECT_FALSE(level.isThereRoom(-1, 0));
+    EXPECT_FALSE(level.isThereRoom(0, 3));
 }
 
-TEST(LevelTest, GenerateMap) {
-    string expectedMap = 
-        "P - R \n"
-        "- - - \n"
-        "R - - \n";
-    EXPECT_EQ(level->generateMap(0, 1), expectedMap);
+TEST(LevelTest, GenerateMap)
+{
+
+    vector<vector<Room*>> roomVector {
+        {new Room("testRoom"), nullptr, new Room("testRoom")},
+        {new Room("testRoom"), new Room("testRoom"), nullptr},
+        {nullptr, new Room("testRoom"), nullptr}
+    };
+    Level level(1, roomVector);
+
+    string expectedMap =
+        "-   - \n"
+        "- P   \n"
+        "  -   \n";
+    EXPECT_EQ(level.generateMap(1, 1), expectedMap);
 }
 
-TEST(LevelTest, SetRoomValid) {
+TEST(LevelTest, SetRoomValid)
+{
+
+    vector<vector<Room*>> roomVector {
+        {new Room("testRoom"), nullptr, new Room("testRoom")},
+        {new Room("testRoom"), new Room("testRoom"), nullptr},
+        {nullptr, new Room("testRoom"), nullptr}
+    };
+    Level level(1, roomVector);
+
     Room* newRoom = new Room("new room");
-    level->setRoom(1, 2, *newRoom);
-    EXPECT_EQ(level->roomVector[1][2], newRoom);
+    level.setRoom(1, 2, newRoom);
+    EXPECT_EQ(level.getRoomAt(1, 2), newRoom);
     delete newRoom;
 }
 
-TEST(LevelTest, SetRoomInvalid) {
-    Room* newRoom = new Room("new room");
-    level->setRoom(3, 0, *newRoom);
-    EXPECT_EQ(level->roomVector[1][2], nullptr);
-    delete newRoom;
-}
+TEST(LevelTest, SetRoomInvalid)
+{
 
+    vector<vector<Room*>> roomVector {
+        {new Room("testRoom"), nullptr, new Room("testRoom")},
+        {new Room("testRoom"), new Room("testRoom"), nullptr},
+        {nullptr, new Room("testRoom"), nullptr}
+    };
+    Level level(1, roomVector);
+
+    Room *newRoom = new Room("new room");
+    EXPECT_DEATH(level.setRoom(3, 0, newRoom), "Invalid coordinates");
+}
